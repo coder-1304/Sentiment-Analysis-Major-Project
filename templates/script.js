dd83ec125ec4ac2a9ac748b8164dc629831808cd
coder-1304 <shanneeahirwar@gmail.com>
2023-10-09 12:46:10 +0530

    UI improvements

--- templates/script.js
+++ templates/script.js
@@ -5,14 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
   analyzeButton.addEventListener("click", function () {
     const text = textInput.value;
-
+    document.getElementById("emotions-meter").style.display = "none"
     const data = {
       text: text,
     };
     console.log(data);
-    var image = document.getElementById("image");
+    var memeImage = document.getElementById("meme-image");
     let color = "";
     let result = "";
+    let bgCol = "";
 
     // Send a POST request to the server
     fetch("http://127.0.0.1:8080/prediction", {
@@ -24,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
     })
       .then((response) => response.json())
       .then((res) => {
+        console.log(res);
         switch (res.predicted_emotion) {
           case "neutral":
             color = "gray";
@@ -32,37 +34,45 @@ document.addEventListener("DOMContentLoaded", function () {
           case "joy":
             color = "green";
             result = "Joy 😄";
+            bgCol = "rgb(232, 255, 232)";
             break;
           case "sadness":
             result = "Sadness 😢";
             color = "rgb(28, 60, 149)";
+            bgCol = "rgb(222, 231, 255)";
             break;
           case "fear":
             result = "Fear 😨";
             color = "rgb(33, 33, 33)";
+            bgCol = "rgb(33, 33, 33)";
             break;
           case "surprise":
             result = "Surprise 😲";
             color = "rgb(205, 205, 0)";
+            bgCol = "rgb(225, 225, 165)";
             break;
           case "anger":
             result = "Anger 😡";
             color = "darkred";
+            bgCol = "rgb(244, 194, 194)"
             break;
           case "shame":
             result = "Shame 😳";
-            color = "pink";
+            color = "rgb(255, 121, 143)";
+            bgCol = "rgb(255, 234, 234)";
             break;
           case "disgust":
             result = "Disgust 🤢";
-            color = "brown";
+            bgCol = "rgb(187, 249, 255)"
+            color = "rgb(0, 223, 186)";
             break;
           default:
             resultText.style.color = "black"; // Default color for other sentiments
         }
-        image.src = `./images/${res.predicted_emotion}.jpg`;
+        memeImage.src = `./images/${res.predicted_emotion}.jpg`;
         resultText.textContent = result;
-        document.body.style.backgroundColor = color;
+        resultText.style.color = color;
+        document.body.style.backgroundColor = bgCol;
       })
       .catch((error) => {
         console.error("Error:", error);
